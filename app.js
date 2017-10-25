b7754460a7f42fbc0874b151614e17067731dcf1
Richard Livingston <livingston.richard@gmail.com>
2017-10-25 13:00:00 -0700

    Fixed my confirms, made them prompts. They will accept upper or lower case with no punctuation.

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 'use strict';
 
-const username = prompt('What\'s your name');
+const username = prompt('What\'s your name?');
 console.log('User said their name was: ' + username);
 alert('Hello ' + username + '!');
+alert(username + ' I am going to ask you a series of yes or no questions. Type yes, no, y or n in the field with no punctuation to submit your answer. Upper or lower case should not matter.')
 
-const likesPizza = confirm(username + ' do I like pizza?'); // Likes pizza?
+const likesPizza = prompt(username + ' do I like pizza?'); // Likes pizza?
 console.log('Question: likes pizza? User answered: ' + likesPizza);
-if(likesPizza === true){
+if(likesPizza.toLowerCase() === 'yes' || likesPizza.toLowerCase() === 'y'){
     alert('Correct! I do like pizza.');
 } else{
     alert('Wrong... who doesn\'t like pizza..?');
 }
 
-const likesGuardians = confirm(username + ' did I enjoy watching Guardians of the Galaxy?'); // Likes Guardians of the Galaxy?
+const likesGuardians = prompt(username + ' did I enjoy watching Guardians of the Galaxy?'); // Likes Guardians of the Galaxy?
 console.log('Question: Liked Guardians of the Galaxy? User answered: ' + likesGuardians);
-if(likesGuardians === true){
+if(likesGuardians.toLowerCase() === 'yes' || likesGuardians.toLowerCase() === 'y'){
     alert('Correct! That movie was awesome.');
 }else{
     alert('Wrong. How could you NOT like that movie?');
 }
 
-const likesGuardiansSequel = confirm(username + ' what about the sequel Guardians 2? Did I like that movie?'); // Likes Guardians 2?
+const likesGuardiansSequel = prompt(username + ' what about the sequel Guardians 2? Did I like that movie?'); // Likes Guardians 2?
 console.log('Question: Did I like the sequel of Guardians of the Galaxy? User answered: ' + likesGuardiansSequel);
-if(likesGuardiansSequel === true){
+if(likesGuardiansSequel.toLowerCase() === 'yes' || likesGuardiansSequel.toLowerCase() === 'y'){
     alert('Correct! Best sequel in years!');
 }else{
     alert('Wrong. Of course I liked the sequel! Best sequel in years imho...');
 }
 
-const favFoodPotatoes = confirm(username + ' would you believe me if I told you potatoes were my favorite food?'); // Likes potatoes?
+const favFoodPotatoes = prompt(username + ' would you believe me if I told you potatoes were my favorite food?'); // Likes potatoes?
 console.log('Question: Are potatoes my favorite food? User answered: ' + favFoodPotatoes);
-if(favFoodPotatoes === true){
+if(favFoodPotatoes.toLowerCase() === 'yes' || favFoodPotatoes.toLowerCase() === 'y'){
     alert('Correct! Potatoes are so versatile.');
 }else{
     alert('Wrong. Potatoes are the best food ever!');
 }
 
-const grewUp = confirm(username + ' did I grow up in Oregon?'); // Grew up in Oregon?
+const grewUp = prompt(username + ' did I grow up in Oregon?'); // Grew up in Oregon?
 console.log('Question: Did I grow in in Oregon? User answered: ' + grewUp);
-if(grewUp === false){
+if(likesGuardians.toLowerCase() === 'no' || likesGuardians.toLowerCase() === 'n'){
     alert('Correct! I actually grew up in Alaska!');
 }else{
     alert('Wrong. How could you have known that tho?');
